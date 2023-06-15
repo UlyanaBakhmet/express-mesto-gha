bf420f827b8c68262acf9fe5a518e680b4675403
Ulyana Bakhmet <buyarova.u@yandex.ru>
2023-06-15 18:13:02 +0300

    13

--- controllers/cards.js
+++ controllers/cards.js
@@ -16,8 +16,10 @@ module.exports.getCards = (req, res) => {
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
+  const owner = req.user._id;
+
   cardSchema
-    .create({ name, link, owner: req.user._id })
+    .create({ name, link, owner })
     .then((card) => res.send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
